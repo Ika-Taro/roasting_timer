a390f7f497ad2932da1349094d1bd09ec50e9271
Ika-Taro <black.jkh@gmail.com>
2022-04-07 17:52:35 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/roasting_timer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.roasting_timer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // カウンター
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    // 1. Timer.periodic : 新しい繰り返しタイマーを作成します
+    // 1秒ごとに _counterを1ずつ足していく
+    Timer.periodic(
+      // 第一引数：繰り返す間隔の時間を設定
+      const Duration(seconds: 1),
+      // 第二引数：その間隔ごとに動作させたい処理を書く
+      (Timer timer) {
+        _counter++;
+        setState(() {});
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('タイマー'),
+      ),
+      body: Center(
+        child: Text(_counter.toString()),
+      ),
+    );
+  }
+
+  Widget panel() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {},
+          child: const SizedBox(height: 40, child: const Icon(Icons.stop)),
+        ),
+        GestureDetector(
+            onTap: () {},
+            child: const SizedBox(height: 40, child: Icon(Icons.play_arrow))),
+      ],
+    );
+  }
+}
