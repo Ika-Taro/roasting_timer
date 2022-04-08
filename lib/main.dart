bbb619c2493520f5e656da0a57643f66fa3a91c5
Ika-Taro <black.jkh@gmail.com>
2022-04-08 19:02:26 +0900

    メモ機能前

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,83 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:roasting_timer/edit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 void main() {
   runApp(const MyApp());
 }
-
+String formatTime(int milliseconds) {
+  var secs = milliseconds ~/ 1000;
+  var hours = (secs ~/ 3600).toString().padLeft(2, '0');
+  var minutes = ((secs % 3600) ~/ 60).toString().padLeft(2, '0');
+  var seconds = (secs % 60).toString().padLeft(2, '0');
+  return "$hours:$minutes:$seconds";
+}
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '焙煎タイマー',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
+    return const MaterialApp(title: 'Stopwatch Example', home: StopwatchPage());
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class StopwatchPage extends StatefulWidget {
+  const StopwatchPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _StopwatchPageState createState() => _StopwatchPageState();
 }
-
-class _MyHomePageState extends State<MyHomePage> {
-  // カウンター
-  int _timer = 0;
-
+class _StopwatchPageState extends State<StopwatchPage> {
+  late Stopwatch _stopwatch;
+  late Timer _timer;
 
   @override
   void initState() {
     super.initState();
-    // 1. Timer.periodic : 新しい繰り返しタイマーを作成します
-    // 1秒ごとに _counterを1ずつ足していく
-    Timer.periodic(
-      // 第一引数：繰り返す間隔の時間を設定
-      const Duration(seconds: 1),
-      // 第二引数：その間隔ごとに動作させたい処理を書く
-          (Timer timer) {
-        _timer++;
-        setState(() {});
-      },
-    );
+    _stopwatch = Stopwatch();
+    // re-render every 30ms
+    _timer = Timer.periodic(const Duration(milliseconds: 30), (timer) {
+      setState(() {});
+    });
+  }
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
   }
 
-
+  void handleStartStop() {
+    if (_stopwatch.isRunning) {
+      _stopwatch.stop();
+    } else {
+      _stopwatch.start();
+    }
+    setState(() {});    // re-render the page
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('焙煎タイマー'),
-      ),
+      appBar: AppBar(title: const Text('Stopwatch Example')),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) {
-                return SecondPage(_timer.toString());
-              }),
-            );
-          },
-          child: Text(_timer.toString()),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(formatTime(_stopwatch.elapsedMilliseconds), style: const TextStyle(fontSize: 48.0)),
+            ElevatedButton(onPressed: handleStartStop, child: Text(_stopwatch.isRunning ? 'Stop' : 'Start')),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) {
+                    return SecondPage(_stopwatch.elapsed.toString());
+                  }),
+                );
+              },
+              child: const Text('保存'),
+            ),
+          ],
         ),
       ),
     );
@@ -75,20 +86,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class SecondPage extends StatelessWidget {
   SecondPage(this.name);
+
   final String name;
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Second Page")),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text(name),
-        ),
-      ),
-    );
+        appBar: AppBar(title: const Text("Second Page")),
+        body: Center()
+          child: Card(
+            child: Column(
+              title: Text(name),
+            ),
+          ),
+        );
   }
 }
-
